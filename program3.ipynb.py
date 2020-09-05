434d28907d7a2d5bb254a4d5314609a3291e1181
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2020-09-05 18:08:21 +0530

    Add files via upload

---
+++ program3.ipynb.py
@@ -0,0 +1,39 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plot
+
+
+seed=[47,89,299,2003,20000]
+
+#given values for lcg
+m=2048
+b=1
+a=1229
+seed=0
+
+x=seed
+
+u_prev=[]
+u_next=[]
+
+for _ in range(m):
+    u=x/m
+    
+    x=(a*x+b)%m
+    
+    if(x==seed):
+        break
+    
+    u=float("{:.2f}".format(u))
+    u_prev.append(u)
+    u=x/m;
+    u=float("{:.2f}".format(u))
+    u_next.append(u)
+    
+
+plot.figure(figsize=(15,15))
+plot.scatter(u_prev,u_next,color='g', ec='b')
+plot.show()
+    
+
+
